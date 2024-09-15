7c9942d1ff67acf0c82ad5d91ffb68bf48e17a14
irfanmohamed12 <irfanakazaid@gmail.com>
2024-09-15 21:37:30 +0100

    240915 switching domains 1

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://irfanmohamed12.github.io',  
+  site: 'https://irfanrefai.com',  
   integrations: [mdx(), sitemap(), tailwind()]
 });
